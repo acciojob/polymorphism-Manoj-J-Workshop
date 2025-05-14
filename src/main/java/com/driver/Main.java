80f0df08a87942bef82c41220f8a5977d5589960
Practice_GitHub <manojj.gowda091298@gmail.com>
2025-05-14 09:16:28 +0530

    Assignment tasks (1-4)

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x, int y) {
+            return (x*y);
+        };
 
+        public int product(int x, int y, int z) {
+            return (x*y*z);
+        };
+        public double product(double x, double y) {
+            return (x*y);
+        };
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        int x =3,y =5,z = 6;
+
+        double a = 3.36,b=7.76;
+//        int res1 = p.product(3,4);
+//        int res2 = p.product(3,4,5);
+//        double res3 = p.product(5.01,7.76);
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(a,b);
+
+        //System.out.println("res1 : "+res1+" res2: "+res2+" res3: "+res3);
+    }
 }
