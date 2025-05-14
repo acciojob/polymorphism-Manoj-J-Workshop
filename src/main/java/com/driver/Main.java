36242db30710cc50b2bcb8bd1e6eeb03c65adf85
Practice_GitHub <manojj.gowda091298@gmail.com>
2025-05-14 09:20:37 +0530

    Assignment tasks (1-4)

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,6 +5,7 @@ public class Main {
         public int product(int x, int y) {
             return (x*y);
         };
+        //dummy lines
 
         public int product(int x, int y, int z) {
             return (x*y*z);
