388e73fc6fe3a09fecb48920923969ba138058c7
Manoj-J-Workshop <manojj91298@gmail.com>
2025-05-18 11:24:16 +0530

    Assignment task 1-4 updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
         public int product(int x, int y) {
             return (x*y);
         };
@@ -15,18 +15,4 @@ public class Main {
         };
     }
 
-    public static void main(String[] args) {
-        Product p = new Product();
-        int x =3,y =5,z = 6;
-
-        double a = 3.36,b=7.76;
-//        int res1 = p.product(3,4);
-//        int res2 = p.product(3,4,5);
-//        double res3 = p.product(5.01,7.76);
-        p.product(x,y);
-        p.product(x,y,z);
-        p.product(a,b);
-
-        //System.out.println("res1 : "+res1+" res2: "+res2+" res3: "+res3);
-    }
 }
